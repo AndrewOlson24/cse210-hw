6487600ec60f5a9ae12c8f104aa92abd21dcc737
AndrewOlson24 <andy.w.olson@gmail.com>
2023-03-18 11:29:43 -0600

    get

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine("Wlecome to the Game of FUN");
+        Console.WriteLine("THis Game has 3 options");
+        Console.WriteLine("1. Breathing Activity");
+        Console.WriteLine("2. Reflection Activty");
+        Console.WriteLine("3. Listing Activity");
+        Console.WriteLine("4. Quit");
+        Console.WriteLine("PLease choose your Activity. ");
+        string playerChos = Console.ReadLine();
+            
+            if(playerChos = 1);
+            {
+            
+            }
+            else if(playerChos = 2);
+            {
+   
+            }
+            else if(playerChos = 3);
+            {
+
+            }
+            else if(playerChos = 4);
+            {
+
+            }
+           else
+           {
+            Console.WriteLine("Sorry, didn't catch that, Please type a number 1-4");
+           }
+
+
     }
+
+public class Reflecting
+{
+prompt = List[]
+Console.WriteLine("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+
+
+
+
+}
+
+public class Breathing
+{
+    public void Breathingagain()
+    {
+        
+        Console.WriteLine("This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
+        Console.WriteLine("Please give me a number? ");
+        bool timeseconds = Console.ReadLine();
+        
+        while(timeseconds >= timeseconds)
+        {
+            Console.WriteLine("Breath in.......")
+
+
+            Console.WriteLine("Breathe out....")
+        }
+        Console.Writeline("Thank you for playing");
+    }
+}
+public class Listing
+{
+
+}
+
+
+
 }
